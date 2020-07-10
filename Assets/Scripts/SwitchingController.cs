df4ad712baa3fb63900fc36433216bfc12672a22
Zenith08 <kaidenr@shaw.ca>
2020-07-10 15:13:04 -0700

    Added switchable tiles for use later.

---
+++ Assets/Scripts/SwitchingController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchingController : MonoBehaviour
+{
+    public void SwitchAllTiles(int newIndex)
+    {
+        GameObject[] switchers = GameObject.FindGameObjectsWithTag("switcher");
+        foreach(GameObject go in switchers)
+        {
+            if(go.GetComponent<SwitchingTile>() != null)
+            {
+                go.GetComponent<SwitchingTile>().ChangeToIndex(newIndex);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchingTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Mode
+{
+    public int index;
+    public Sprite sprite;
+    public bool colliderEnabled;
+}
+
+public class SwitchingTile : MonoBehaviour
+{
+    public List<Mode> modes;
+    public int currentMode;
+
+    private SpriteRenderer sr;
+    private Collider2D assetCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        assetCollider = GetComponent<Collider2D>();
+    }
+
+    public void ChangeToIndex(int index)
+    {
+        if(index >= modes.Count)
+        {
+            index = 0;
+        }
+        currentMode = index;
+        sr.sprite = modes[index].sprite;
+        assetCollider.enabled = modes[index].colliderEnabled;
+    }
+}
