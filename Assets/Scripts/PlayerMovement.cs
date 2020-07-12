2095c2ff9a187cc48cab2a729d583b52dbf250bd
PurpleFumetsu <46788247+PurpleFumetsu@users.noreply.github.com>
2020-07-11 17:36:14 -0700

    added death sfx

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -67,6 +67,7 @@ public class PlayerMovement : MonoBehaviour
     private void DoDeath()
     {
         //Play sound effect.
+        SoundManagerScript.PlaySound("death");
         GameObject.FindGameObjectWithTag("Portal").GetComponent<LevelLoader>().ReloadLevel();
     }
 
